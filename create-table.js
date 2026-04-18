c6a4743025aa2c95a357ac0c709b05c6d4f328ce
Chambatina1 <geochambatina@gmail.com>
2026-04-18 10:17:45 -0400

    Update create-table.js

--- create-table.js
+++ create-table.js
@@ -1,4 +1,5 @@
-const { Client } = require('pg');
+import pkg from 'pg';
+const { Client } = pkg;
 
 async function crearTabla() {
   const client = new Client({
@@ -10,13 +11,15 @@ async function crearTabla() {
     await client.connect();
     console.log('Conectado');
 
-  await client.query(`
-  CREATE TABLE IF NOT EXISTS orders (
-    id SERIAL PRIMARY KEY,
-    buyer_first_name TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  );
-`);
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS orders (
+        id SERIAL PRIMARY KEY,
+
+        buyer_first_name TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
     console.log('Tabla creada');
   } catch (err) {
     console.error(err);
